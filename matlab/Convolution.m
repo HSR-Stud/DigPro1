cc735d8351cf2442e034a9de037857c3aa06d563
Juerg Rast <juergr@gmail.com>
2013-01-22 14:06:59 +0100

    Code Normale Faltung/FTT

---
+++ matlab/Convolution.m
@@ -0,0 +1,23 @@
+f = [zeros(1,10) ones(1,5) zeros(1,10)];
+h = [zeros(1,10) ones(1,5) zeros(1,10)];
+
+lf = length(f);
+lh = length(h);
+lg = lf+lh-1;
+g = zeros(1,lg);
+
+for x = 1:lf+lh-1
+    t = 0;
+    start = max(1,x+1-lh);
+    ende = min(x,lh);
+    for y = start:ende
+        t = t + f(y)*h(x-y+1);
+    end
+    g(x) = t;
+end
+subplot(2,1,1)
+stem(g);title('Convolution');grid on;
+
+subplot(2,1,2)
+result=real(ifft(fft(f).*fft(h)));
+stem(result); title('Circular Convolution');grid on;
